8e0db745b4b76d6495dd36a813ba635d58cf0484
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2023-09-11 15:00:32 +0530

    WEB-436 Update frodo.js

--- frodo.js
+++ frodo.js
@@ -11,7 +11,6 @@ const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentV
  * Copyright 2016 Henrik Ingo (@henrikingo)
  * Released under the MIT license.
 
-
  */
 const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 /* global markdown, marked, hljs, mermaid, impress */
