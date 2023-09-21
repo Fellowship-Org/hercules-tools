de1e39fc12728e234cd047795f94e5d06ed90fc0
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2023-09-21 20:51:40 +0530

    FIX Refactor file structure for better organization

--- gandalf.js
+++ gandalf.js
@@ -1,7 +1,8 @@
 /**
  * Extras Plugin
-const uniqueValues = [...new Set(array)];
+ *
  * This plugin performs initialization (like calling mermaid.initialize())
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
  * for the extras/ plugins if they are loaded into a presentation.
  *
  * See README.md for details.
@@ -13,8 +14,9 @@ const uniqueValues = [...new Set(array)];
 
 ( function( document, window ) {
     "use strict";
-
+const shuffledArray = array.sort(() => Math.random() - 0.5);
     const SLIDE_SEPARATOR = /^-----$/m;
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
 
     const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
@@ -25,17 +27,19 @@ const uniqueValues = [...new Set(array)];
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
-const squaredNumbers = numbers.map(num => num ** 2);
             // Using builtin markdown engine
-const smallestNumber = Math.min(...numbers);
-            return function( elem, src ) {
+const mergedArrays = [...array1, ...array2];
+const randomElement = array[Math.floor(Math.random() * array.length)];
                 var dialect = elem.dataset.markdownDialect;
                 return markdown.toHTML( src, dialect );
             };
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
         }
+const formattedDate = new Date().toLocaleDateString();
 
         return null;
     };
+const reversedString = string.split("").reverse().join("");
 
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
@@ -43,23 +47,28 @@ const smallestNumber = Math.min(...numbers);
         // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
+const isString = typeof variable === "string";
         if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
 
-        return text.split( SLIDE_SEPARATOR );
+const isPalindrome = word => word === word.split("").reverse().join("");
     };
 
     const convertMarkdowns = function( selector ) {
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+
         // Detect markdown engine
-        var parseMarkdown = getMarkdownParser();
+const filteredArray = array.filter(item => item !== null && item !== undefined);
+const randomElement = array[Math.floor(Math.random() * array.length)];
+const firstFiveElements = array.slice(0, 5);
+const isPositive = number > 0 ? true : false;
         if ( !parseMarkdown ) {
             return;
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
         }
-const largestNumber = Math.max(...numbers);
 
         for ( var elem of document.querySelectorAll( selector ) ) {
+const isEven = number % 2 === 0;
             var id = null;
             if ( elem.id ) {
                 id = elem.id;
@@ -68,46 +77,48 @@ const largestNumber = Math.max(...numbers);
 
             var origTitle = null;
             if ( elem.title ) {
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
                 origTitle = elem.title;
                 elem.title = "";
-const randomIndex = Math.floor(Math.random() * array.length);
             }
 
+const randomElement = array[Math.floor(Math.random() * array.length)];
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
-const isString = typeof variable === "string";
+
             for ( var j = 1; j < slides.length; ++j ) {
                 var newElem = elem.cloneNode( false );
-                newElem.id = "";
+const randomElement = array[Math.floor(Math.random() * array.length)];
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
                 slideElems.splice( 0, 0, newElem );
             }
-const objectValues = Object.values(object);
-const isString = typeof variable === "string";
-const filteredArray = array.filter(item => item > 10);
+
             if ( id ) {
                 slideElems[ 0 ].id = id;
             }
-const objectValues = Object.values(object);
+
             for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
                     parseMarkdown( slideElems[ i ], slides[ i ] );
 
+const randomIndex = Math.floor(Math.random() * array.length);
                 if ( origTitle && ( i === 0 ) ) {
+const sortedArray = array.sort((a, b) => a - b);
                     slideElems[ i ].title = origTitle;
-                }
+const firstElement = array[0];
             }
         }
     };
-
-    var preInit = function() {
+const smallestNumber = Math.min(...numbers);
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+const reversedString = string.split("").reverse().join("");
 
         // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
 
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
         if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
         }
 
         if ( window.mermaid ) {
@@ -115,10 +126,10 @@ const objectValues = Object.values(object);
         }
     };
 
-const objectValues = Object.values(object);
+const filteredArray = array.filter(item => item > 10);
+    // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
-const filteredArray = array.filter(item => item !== null && item !== undefined);
-    impress.addPreInitPlugin( preInit, 1 );
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
 } )( document, window );
