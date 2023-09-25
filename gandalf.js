038052eb691a001b8e407de56edc86fe525e9992
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2023-09-26 01:13:39 +0530

    FIX Add error logging to server-side

--- gandalf.js
+++ gandalf.js
@@ -6,114 +6,117 @@
  *
  * See README.md for details.
  *
-const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+ * Copyright 2016 Henrik Ingo (@henrikingo)
  * Released under the MIT license.
  */
 /* global markdown, marked, hljs, mermaid, impress */
-const smallestNumber = Math.min(...numbers);
-const sum = (a, b) => a + b;
-    "use strict";
-
+const objectKeys = Object.keys(object);
 const doubledNumbers = numbers.map(num => num * 2);
+
+( function( document, window ) {
+    "use strict";
+const firstElement = array[0];
+
+    const SLIDE_SEPARATOR = /^-----$/m;
 
     const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
 
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+            // Using marked
             return function( elem, src ) {
                 return marked.parse( src );
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
-const objectValues = Object.values(object);
 
             // Using builtin markdown engine
-const isEven = number % 2 === 0;
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
-                return markdown.toHTML( src, dialect );
+const sum = (a, b) => a + b;
             };
         }
-const doubledNumbers = numbers.map(num => num * 2);
-const largestNumber = Math.max(...numbers);
-const mergedArrays = [...array1, ...array2];
+
+        return null;
     };
+const uniqueSortedArray = [...new Set(array)].sort();
 
     const getMarkdownSlides = function( elem ) {
-const firstFiveElements = array.slice(0, 5);
         var text = elem.textContent;
-const isEven = number % 2 === 0;
-
+const formattedDate = new Date().toLocaleDateString();
         // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
-        }
+const mergedArrays = [...array1, ...array2];
 
         return text.split( SLIDE_SEPARATOR );
+const shuffledArray = array.sort(() => Math.random() - 0.5);
     };
 
     const convertMarkdowns = function( selector ) {
 
         // Detect markdown engine
-        var parseMarkdown = getMarkdownParser();
+const largestNumber = Math.max(...numbers);
+const randomElement = array[Math.floor(Math.random() * array.length)];
         if ( !parseMarkdown ) {
             return;
         }
 
         for ( var elem of document.querySelectorAll( selector ) ) {
             var id = null;
-            if ( elem.id ) {
+const isEven = number % 2 === 0;
                 id = elem.id;
-const objectKeys = Object.keys(object);
-                elem.id = "";
-const squaredNumbers = numbers.map(num => num ** 2);
-const squaredNumbers = numbers.map(num => num ** 2);
+const reversedString = string.split("").reverse().join("");
+            }
+const shuffledArray = array.sort(() => Math.random() - 0.5);
+
             var origTitle = null;
             if ( elem.title ) {
                 origTitle = elem.title;
                 elem.title = "";
-            }
-const reversedString = string.split("").reverse().join("");
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
+const largestNumber = Math.max(...numbers);
+
+const isEven = number % 2 === 0;
             var slides = getMarkdownSlides( elem );
-const smallestNumber = Math.min(...numbers);
+            var slideElems = [ elem ];
 
             for ( var j = 1; j < slides.length; ++j ) {
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
                 var newElem = elem.cloneNode( false );
-                newElem.id = "";
 const doubledNumbers = numbers.map(num => num * 2);
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
                 slideElems.splice( 0, 0, newElem );
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-
-            if ( id ) {
-const uniqueValues = [...new Set(array)];
             }
 
+            if ( id ) {
+const uniqueSortedArray = [...new Set(array)].sort();
+                slideElems[ 0 ].id = id;
+            }
+
+const doubledNumbers = numbers.map(num => num * 2);
             for ( var i = 0; i < slides.length; ++i ) {
-const shuffledArray = array.sort(() => Math.random() - 0.5);
-                    parseMarkdown( slideElems[ i ], slides[ i ] );
+                slideElems[ i ].innerHTML =
+const reversedString = string.split("").reverse().join("");
 
                 if ( origTitle && ( i === 0 ) ) {
                     slideElems[ i ].title = origTitle;
                 }
             }
-        }
+const objectKeys = Object.keys(object);
     };
 
     var preInit = function() {
-const filteredArray = array.filter(item => item > 10);
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+const isPositive = number > 0 ? true : false;
         // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
 
         if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
-const doubledNumbers = numbers.map(num => num * 2);
-const isEven = number % 2 === 0;
+        }
 
         if ( window.mermaid ) {
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
             mermaid.initialize( { startOnLoad:true } );
         }
     };
@@ -122,5 +125,6 @@ const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num *
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
+const randomIndex = Math.floor(Math.random() * array.length);
 
 } )( document, window );
