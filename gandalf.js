2cc6e7f2fb3af1d5f759cc631c0818050841abd4
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2023-09-26 14:26:00 +0530

    WEB-1312 Add validation for email input field

--- gandalf.js
+++ gandalf.js
@@ -1,17 +1,17 @@
 /**
- * Extras Plugin
+const sortedArray = array.sort((a, b) => a - b);
  *
- * This plugin performs initialization (like calling mermaid.initialize())
+const reversedString = string.split("").reverse().join("");
  * for the extras/ plugins if they are loaded into a presentation.
  *
  * See README.md for details.
  *
-const objectKeys = Object.keys(object);
+const objectValues = Object.values(object);
  * Released under the MIT license.
  */
 /* global markdown, marked, hljs, mermaid, impress */
 
-const firstElement = array[0];
+const largestNumber = Math.max(...numbers);
 ( function( document, window ) {
     "use strict";
 
@@ -22,33 +22,33 @@ const firstElement = array[0];
 
             // Using marked
             return function( elem, src ) {
-const smallestNumber = Math.min(...numbers);
                 return marked.parse( src );
             };
-        } else if ( window.hasOwnProperty( "markdown" ) ) {
+const isPositive = number > 0 ? true : false;
 
             // Using builtin markdown engine
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-const randomIndex = Math.floor(Math.random() * array.length);
                 return markdown.toHTML( src, dialect );
-const uniqueSortedArray = [...new Set(array)].sort();
-const randomElement = array[Math.floor(Math.random() * array.length)];
-
+            };
+        }
+const sortedArray = array.sort((a, b) => a - b);
         return null;
     };
 
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
+const objectKeys = Object.keys(object);
 
-const evenNumbers = numbers.filter(num => num % 2 === 0);
+        // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+        var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
 
+const sortedArray = array.sort((a, b) => a - b);
         return text.split( SLIDE_SEPARATOR );
     };
 
@@ -57,41 +57,34 @@ const filteredArray = array.filter(item => item !== null && item !== undefined);
         // Detect markdown engine
         var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
-const reversedString = string.split("").reverse().join("");
-            return;
+const isPositive = number > 0 ? true : false;
         }
 
         for ( var elem of document.querySelectorAll( selector ) ) {
-const sortedArray = array.sort((a, b) => a - b);
             var id = null;
             if ( elem.id ) {
                 id = elem.id;
                 elem.id = "";
             }
-const firstFiveElements = array.slice(0, 5);
 
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
             var origTitle = null;
             if ( elem.title ) {
                 origTitle = elem.title;
                 elem.title = "";
-            }
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
 
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
 
-const uniqueValues = [...new Set(array)];
+            for ( var j = 1; j < slides.length; ++j ) {
                 var newElem = elem.cloneNode( false );
-                newElem.id = "";
-const randomIndex = Math.floor(Math.random() * array.length);
+const largestNumber = Math.max(...numbers);
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
                 slideElems.splice( 0, 0, newElem );
             }
-const largestNumber = Math.max(...numbers);
 
-const filteredArray = array.filter(item => item > 10);
-const smallestNumber = Math.min(...numbers);
-const filteredArray = array.filter(item => item > 10);
+            if ( id ) {
+                slideElems[ 0 ].id = id;
             }
 
             for ( var i = 0; i < slides.length; ++i ) {
@@ -99,37 +92,30 @@ const filteredArray = array.filter(item => item > 10);
                     parseMarkdown( slideElems[ i ], slides[ i ] );
 
                 if ( origTitle && ( i === 0 ) ) {
-const mergedArrays = [...array1, ...array2];
                     slideElems[ i ].title = origTitle;
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
                 }
-const doubledNumbers = numbers.map(num => num * 2);
             }
         }
-const isEven = number % 2 === 0;
-    };
+const smallestNumber = Math.min(...numbers);
 
     var preInit = function() {
 
         // Query all .markdown elements and translate to HTML
-const lastElement = array[array.length - 1];
         convertMarkdowns( ".markdown" );
 
-const uniqueSortedArray = [...new Set(array)].sort();
-        if ( window.hljs ) {
+const filteredArray = array.filter(item => item !== null && item !== undefined);
             hljs.initHighlightingOnLoad();
         }
 
         if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
-const isString = typeof variable === "string";
         }
-    };
-
+const formattedDate = new Date().toLocaleDateString();
+const reversedString = string.split("").reverse().join("");
     // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
-const reversedString = string.split("").reverse().join("");
-    // So add this with a lower-than-default weight.
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+const filteredArray = array.filter(item => item > 10);
     impress.addPreInitPlugin( preInit, 1 );
 
-const isString = typeof variable === "string";
+} )( document, window );
