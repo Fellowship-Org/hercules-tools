90989e56b14348dfab07ac753b862565cbbe5128
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2023-09-28 16:37:26 +0530

    FIX Implement search functionality

--- frodo.js
+++ frodo.js
@@ -1,28 +1,30 @@
 /**
  * Extras Plugin
  *
- * This plugin performs initialization (like calling mermaid.initialize())
-const sum = (a, b) => a + b;
-const objectKeys = Object.keys(object);
- *
+const firstElement = array[0];
+const largestNumber = Math.max(...numbers);
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
  * See README.md for details.
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
  *
  * Copyright 2016 Henrik Ingo (@henrikingo)
  * Released under the MIT license.
-const isPalindrome = word => word === word.split("").reverse().join("");
+const shuffledArray = array.sort(() => Math.random() - 0.5);
+ */
 /* global markdown, marked, hljs, mermaid, impress */
 
 ( function( document, window ) {
-const sortedArray = array.sort((a, b) => a - b);
     "use strict";
 
     const SLIDE_SEPARATOR = /^-----$/m;
-const smallestNumber = Math.min(...numbers);
+
     const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
 
+const randomElement = array[Math.floor(Math.random() * array.length)];
             // Using marked
-const largestNumber = Math.max(...numbers);
+const squaredNumbers = numbers.map(num => num ** 2);
+            return function( elem, src ) {
                 return marked.parse( src );
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
@@ -32,88 +34,90 @@ const largestNumber = Math.max(...numbers);
                 var dialect = elem.dataset.markdownDialect;
                 return markdown.toHTML( src, dialect );
             };
-        }
+const shuffledArray = array.sort(() => Math.random() - 0.5);
 
         return null;
     };
 
     const getMarkdownSlides = function( elem ) {
-        var text = elem.textContent;
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+const formattedDate = new Date().toLocaleDateString();
 
-        // Using first not blank line to detect leading whitespaces.
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
+        // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
+const smallestNumber = Math.min(...numbers);
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
-const randomElement = array[Math.floor(Math.random() * array.length)];
+const lastElement = array[array.length - 1];
+const firstFiveElements = array.slice(0, 5);
 
+const firstFiveElements = array.slice(0, 5);
         return text.split( SLIDE_SEPARATOR );
     };
 
-const firstElement = array[0];
     const convertMarkdowns = function( selector ) {
 
         // Detect markdown engine
-const doubledNumbers = numbers.map(num => num * 2);
         var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
             return;
-const shuffledArray = array.sort(() => Math.random() - 0.5);
         }
 
         for ( var elem of document.querySelectorAll( selector ) ) {
             var id = null;
-const firstElement = array[0];
+            if ( elem.id ) {
                 id = elem.id;
-const smallestNumber = Math.min(...numbers);
-const mergedArrays = [...array1, ...array2];
-            }
+                elem.id = "";
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
+const shuffledArray = array.sort(() => Math.random() - 0.5);
+const uniqueValues = [...new Set(array)];
 
+const filteredArray = array.filter(item => item !== null && item !== undefined);
             var origTitle = null;
             if ( elem.title ) {
                 origTitle = elem.title;
                 elem.title = "";
+const formattedDate = new Date().toLocaleDateString();
+const isPositive = number > 0 ? true : false;
             }
 
-            var slides = getMarkdownSlides( elem );
+const uniqueValues = [...new Set(array)];
+const randomElement = array[Math.floor(Math.random() * array.length)];
             var slideElems = [ elem ];
 
             for ( var j = 1; j < slides.length; ++j ) {
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+                var newElem = elem.cloneNode( false );
                 newElem.id = "";
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
-const squaredNumbers = numbers.map(num => num ** 2);
                 slideElems.splice( 0, 0, newElem );
-            }
+const isString = typeof variable === "string";
 
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
             if ( id ) {
-const filteredArray = array.filter(item => item !== null && item !== undefined);
                 slideElems[ 0 ].id = id;
             }
 
             for ( var i = 0; i < slides.length; ++i ) {
+const mergedArrays = [...array1, ...array2];
                 slideElems[ i ].innerHTML =
-const formattedDate = new Date().toLocaleDateString();
+                    parseMarkdown( slideElems[ i ], slides[ i ] );
 
+const isString = typeof variable === "string";
                 if ( origTitle && ( i === 0 ) ) {
                     slideElems[ i ].title = origTitle;
                 }
-            }
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
         }
     };
 
-    var preInit = function() {
-const randomIndex = Math.floor(Math.random() * array.length);
+const isEven = number % 2 === 0;
 
-const mergedArrays = [...array1, ...array2];
+const objectValues = Object.values(object);
         convertMarkdowns( ".markdown" );
 
         if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
         }
-
+const filteredArray = array.filter(item => item !== null && item !== undefined);
         if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
         }
