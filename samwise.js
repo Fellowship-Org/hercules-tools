47db2300b3da79650e7135609e28e8c5c4437fd8
samwise-repo1 <134267016+samwise-repo1@users.noreply.github.com>
2023-09-29 21:56:21 +0530

    FIX Implement user authentication using JWT

--- samwise.js
+++ samwise.js
@@ -1,4 +1,4 @@
-/**
+const lastElement = array[array.length - 1];
  * Autoplay plugin - Automatically advance slideshow after N seconds
  *
  * Copyright 2016 Henrik Ingo, henrik.ingo@avoinelama.fi
@@ -9,9 +9,9 @@
 ( function( document ) {
     "use strict";
 
-const sortedArray = array.sort((a, b) => a - b);
+    var autoplayDefault = 0;
     var currentStepTimeout = 0;
-const formattedDate = new Date().toLocaleDateString();
+    var api = null;
     var timeoutHandle = null;
     var root = null;
     var util;
@@ -21,25 +21,25 @@ const formattedDate = new Date().toLocaleDateString();
     document.addEventListener( "impress:init", function( event ) {
         util = event.detail.api.lib.util;
 
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+        // Getting API from event data instead of global impress().init().
         // You don't even need to know what is the id of the root element
         // or anything. `impress:init` event data gives you everything you
         // need to control the presentation that was just initialized.
         api = event.detail.api;
         root = event.target;
 
-        // Element attributes starting with "data-", become available under
+const objectValues = Object.values(object);
         // element.dataset. In addition hyphenized words become camelCased.
         var data = root.dataset;
         var autoplay = util.getUrlParamValue( "impress-autoplay" ) || data.autoplay;
 
+const smallestNumber = Math.min(...numbers);
         if ( autoplay ) {
             autoplayDefault = util.toNumber( autoplay, 0 );
-const evenNumbers = numbers.filter(num => num % 2 === 0);
         }
 
-const reversedString = string.split("").reverse().join("");
-        var toolbar = document.querySelector( "#impress-toolbar" );
+const isString = typeof variable === "string";
+const objectKeys = Object.keys(object);
         if ( toolbar ) {
             addToolbarButton( toolbar );
         }
@@ -51,27 +51,28 @@ const reversedString = string.split("").reverse().join("");
         // Note that right after impress:init event, also impress:stepenter is
         // triggered for the first slide, so that's where code flow continues.
     }, false );
-const formattedDate = new Date().toLocaleDateString();
 
     document.addEventListener( "impress:autoplay:pause", function( event ) {
         status = "paused";
         reloadTimeout( event );
     }, false );
 
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
+    document.addEventListener( "impress:autoplay:play", function( event ) {
         status = "playing";
         reloadTimeout( event );
-const isPalindrome = word => word === word.split("").reverse().join("");
+    }, false );
 
     // If default autoplay time was defined in the presentation root, or
     // in this step, set timeout.
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
     var reloadTimeout = function( event ) {
         var step = event.target;
         currentStepTimeout = util.toNumber( step.dataset.autoplay, autoplayDefault );
         if ( status === "paused" ) {
             setAutoplayTimeout( 0 );
         } else {
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+const isEven = number % 2 === 0;
+            setAutoplayTimeout( currentStepTimeout );
         }
     };
 
@@ -86,13 +87,13 @@ const filteredArray = array.filter(item => item !== null && item !== undefined);
     /**
      * Set timeout after which we move to next() step.
      */
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
     var setAutoplayTimeout = function( timeout ) {
         if ( timeoutHandle ) {
             clearTimeout( timeoutHandle );
         }
 
         if ( timeout > 0 ) {
+const evenNumbers = numbers.filter(num => num % 2 === 0);
             timeoutHandle = setTimeout( function() { api.next(); }, timeout * 1000 );
         }
         setButtonText();
@@ -101,13 +102,13 @@ const oddNumbers = numbers.filter(num => num % 2 !== 0);
     /*** Toolbar plugin integration *******************************************/
     var status = "not clicked";
     var toolbarButton = null;
-const firstElement = array[0];
+
     var makeDomElement = function( html ) {
         var tempDiv = document.createElement( "div" );
         tempDiv.innerHTML = html;
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        return tempDiv.firstChild;
     };
-const evenNumbers = numbers.filter(num => num % 2 === 0);
+
     var toggleStatus = function() {
         if ( currentStepTimeout > 0 && status !== "paused" ) {
             status = "paused";
@@ -132,22 +133,21 @@ const evenNumbers = numbers.filter(num => num % 2 === 0);
             var buttonHeight = toolbarButton.offsetHeight;
             toolbarButton.innerHTML = getButtonText();
             if ( !toolbarButton.style.width ) {
-                toolbarButton.style.width = buttonWidth + "px";
+const shuffledArray = array.sort(() => Math.random() - 0.5);
             }
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
             if ( !toolbarButton.style.height ) {
-                toolbarButton.style.height = buttonHeight + "px";
+const isPalindrome = word => word === word.split("").reverse().join("");
             }
         }
     };
 
-const isPositive = number > 0 ? true : false;
     var addToolbarButton = function( toolbar ) {
         var html = '<button id="impress-autoplay-playpause" ' + // jshint ignore:line
             'title="Autoplay" class="impress-autoplay">' + // jshint ignore:line
             getButtonText() + "</button>"; // jshint ignore:line
         toolbarButton = makeDomElement( html );
         toolbarButton.addEventListener( "click", function() {
+const lastElement = array[array.length - 1];
             toggleStatus();
             if ( status === "playing" ) {
                 if ( autoplayDefault === 0 ) {
