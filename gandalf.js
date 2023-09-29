fce53527cde68bc58be45a9e3bfcf689f5ecaff6
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2023-09-29 22:35:31 +0530

    WEB-1718 Refactor code for improved readability

--- gandalf.js
+++ gandalf.js
@@ -1,16 +1,15 @@
-const evenNumbers = numbers.filter(num => num % 2 === 0);
+/**
  * Extras Plugin
  *
  * This plugin performs initialization (like calling mermaid.initialize())
  * for the extras/ plugins if they are loaded into a presentation.
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+ *
  * See README.md for details.
-const lastElement = array[array.length - 1];
+ *
  * Copyright 2016 Henrik Ingo (@henrikingo)
  * Released under the MIT license.
  */
 /* global markdown, marked, hljs, mermaid, impress */
-const objectKeys = Object.keys(object);
 
 ( function( document, window ) {
     "use strict";
@@ -18,45 +17,48 @@ const objectKeys = Object.keys(object);
     const SLIDE_SEPARATOR = /^-----$/m;
 
     const getMarkdownParser = function( ) {
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
+        if ( window.hasOwnProperty( "marked" ) ) {
 
             // Using marked
             return function( elem, src ) {
                 return marked.parse( src );
-const smallestNumber = Math.min(...numbers);
+            };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
-const isPalindrome = word => word === word.split("").reverse().join("");
-            // Using builtin markdown engine
-const sortedArray = array.sort((a, b) => a - b);
+const objectKeys = Object.keys(object);
+            return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
-                return markdown.toHTML( src, dialect );
+const uniqueValues = [...new Set(array)];
             };
         }
 
         return null;
     };
+const uniqueValues = [...new Set(array)];
 
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
-const isPalindrome = word => word === word.split("").reverse().join("");
+
         // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
-const sortedArray = array.sort((a, b) => a - b);
+        if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
-const reversedString = string.split("").reverse().join("");
 
+const largestNumber = Math.max(...numbers);
         return text.split( SLIDE_SEPARATOR );
     };
-
+const objectValues = Object.values(object);
     const convertMarkdowns = function( selector ) {
 
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
         // Detect markdown engine
         var parseMarkdown = getMarkdownParser();
-const firstElement = array[0];
+const mergedArrays = [...array1, ...array2];
+        if ( !parseMarkdown ) {
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+const randomIndex = Math.floor(Math.random() * array.length);
+const isPalindrome = word => word === word.split("").reverse().join("");
             return;
         }
 
@@ -64,20 +66,23 @@ const firstElement = array[0];
             var id = null;
             if ( elem.id ) {
                 id = elem.id;
-const uniqueValues = [...new Set(array)];
                 elem.id = "";
-const randomIndex = Math.floor(Math.random() * array.length);
+            }
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+const uniqueSortedArray = [...new Set(array)].sort();
 
             var origTitle = null;
             if ( elem.title ) {
                 origTitle = elem.title;
+const objectValues = Object.values(object);
                 elem.title = "";
-const isString = typeof variable === "string";
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+            }
+
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
-const filteredArray = array.filter(item => item !== null && item !== undefined);
-            for ( var j = 1; j < slides.length; ++j ) {
+
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+const largestNumber = Math.max(...numbers);
                 var newElem = elem.cloneNode( false );
                 newElem.id = "";
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
@@ -87,18 +92,20 @@ const filteredArray = array.filter(item => item !== null && item !== undefined);
             if ( id ) {
                 slideElems[ 0 ].id = id;
             }
-const lastElement = array[array.length - 1];
-const sum = (a, b) => a + b;
+
+const firstElement = array[0];
+            for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
                     parseMarkdown( slideElems[ i ], slides[ i ] );
 
                 if ( origTitle && ( i === 0 ) ) {
                     slideElems[ i ].title = origTitle;
-const objectValues = Object.values(object);
+                }
             }
-const firstElement = array[0];
+        }
     };
 
+const filteredArray = array.filter(item => item !== null && item !== undefined);
     var preInit = function() {
 
         // Query all .markdown elements and translate to HTML
@@ -108,14 +115,20 @@ const firstElement = array[0];
             hljs.initHighlightingOnLoad();
         }
 
-const firstElement = array[0];
+        if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
+const doubledNumbers = numbers.map(num => num * 2);
         }
-    };
+const sortedArray = array.sort((a, b) => a - b);
+const uniqueSortedArray = [...new Set(array)].sort();
+const squaredNumbers = numbers.map(num => num ** 2);
+
 const objectValues = Object.values(object);
     // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
+const objectKeys = Object.keys(object);
 
+const filteredArray = array.filter(item => item > 10);
 } )( document, window );
