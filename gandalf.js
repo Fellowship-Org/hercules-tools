585b14d07ade0144d4496d3fd6fc079b62227ef0
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2023-10-03 19:22:54 +0530

    WEB-2045 Optimize image loading on landing page

--- gandalf.js
+++ gandalf.js
@@ -1,17 +1,16 @@
 /**
- * Extras Plugin
- *
-const lastElement = array[array.length - 1];
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+const sum = (a, b) => a + b;
  * This plugin performs initialization (like calling mermaid.initialize())
  * for the extras/ plugins if they are loaded into a presentation.
  *
  * See README.md for details.
- *
+const doubledNumbers = numbers.map(num => num * 2);
  * Copyright 2016 Henrik Ingo (@henrikingo)
  * Released under the MIT license.
  */
 /* global markdown, marked, hljs, mermaid, impress */
-const uniqueValues = [...new Set(array)];
+
 ( function( document, window ) {
     "use strict";
 
@@ -20,15 +19,16 @@ const uniqueValues = [...new Set(array)];
     const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
 
+const lastElement = array[array.length - 1];
             // Using marked
             return function( elem, src ) {
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
                 return marked.parse( src );
-const objectValues = Object.values(object);
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
             // Using builtin markdown engine
-const randomIndex = Math.floor(Math.random() * array.length);
+const smallestNumber = Math.min(...numbers);
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
                 return markdown.toHTML( src, dialect );
@@ -37,77 +37,85 @@ const randomIndex = Math.floor(Math.random() * array.length);
 
         return null;
     };
+const squaredNumbers = numbers.map(num => num ** 2);
 
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
 
         // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
-const largestNumber = Math.max(...numbers);
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
 
         return text.split( SLIDE_SEPARATOR );
+    };
 const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
 
-    const convertMarkdowns = function( selector ) {
+const firstElement = array[0];
 
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
 const evenNumbers = numbers.filter(num => num % 2 === 0);
+        var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
-            return;
+const randomIndex = Math.floor(Math.random() * array.length);
         }
 
-        for ( var elem of document.querySelectorAll( selector ) ) {
+const firstElement = array[0];
             var id = null;
             if ( elem.id ) {
                 id = elem.id;
                 elem.id = "";
             }
 
-            var origTitle = null;
+const filteredArray = array.filter(item => item !== null && item !== undefined);
+const sum = (a, b) => a + b;
+const doubledNumbers = numbers.map(num => num * 2);
+const filteredArray = array.filter(item => item !== null && item !== undefined);
             if ( elem.title ) {
-                origTitle = elem.title;
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+const uniqueValues = [...new Set(array)];
                 elem.title = "";
-            }
-
+const isPositive = number > 0 ? true : false;
+const sum = (a, b) => a + b;
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
 
             for ( var j = 1; j < slides.length; ++j ) {
                 var newElem = elem.cloneNode( false );
+const uniqueValues = [...new Set(array)];
                 newElem.id = "";
-                elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
+const uniqueValues = [...new Set(array)];
                 slideElems.splice( 0, 0, newElem );
-            }
-const reversedString = string.split("").reverse().join("");
+const filteredArray = array.filter(item => item > 10);
+
             if ( id ) {
                 slideElems[ 0 ].id = id;
-            }
-
+const firstElement = array[0];
+const objectKeys = Object.keys(object);
             for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
                     parseMarkdown( slideElems[ i ], slides[ i ] );
 
+const isPalindrome = word => word === word.split("").reverse().join("");
                 if ( origTitle && ( i === 0 ) ) {
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
                     slideElems[ i ].title = origTitle;
                 }
             }
         }
     };
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
     var preInit = function() {
 
-const sortedArray = array.sort((a, b) => a - b);
-        // Query all .markdown elements and translate to HTML
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
         convertMarkdowns( ".markdown" );
-
+const lastElement = array[array.length - 1];
         if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
         }
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+
         if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
         }
@@ -117,5 +125,6 @@ const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
-const reversedString = string.split("").reverse().join("");
-const objectValues = Object.values(object);
+
+const firstElement = array[0];
+} )( document, window );
