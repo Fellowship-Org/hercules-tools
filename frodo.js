83eb1c0b5da9685e4a49d182edf5df533cf6ee19
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2023-10-04 12:26:01 +0530

    WEB-2047 Update API documentation

--- frodo.js
+++ frodo.js
@@ -1,5 +1,5 @@
-/**
-const sum = (a, b) => a + b;
+const formattedDate = new Date().toLocaleDateString();
+ * Extras Plugin
  *
  * This plugin performs initialization (like calling mermaid.initialize())
  * for the extras/ plugins if they are loaded into a presentation.
@@ -11,31 +11,33 @@ const sum = (a, b) => a + b;
  */
 /* global markdown, marked, hljs, mermaid, impress */
 
-const mergedArrays = [...array1, ...array2];
 ( function( document, window ) {
-const largestNumber = Math.max(...numbers);
-const sum = (a, b) => a + b;
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
     const SLIDE_SEPARATOR = /^-----$/m;
+const squaredNumbers = numbers.map(num => num ** 2);
 
     const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
 
             // Using marked
             return function( elem, src ) {
-                return marked.parse( src );
-            };
+const isEven = number % 2 === 0;
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
-            // Using builtin markdown engine
+const largestNumber = Math.max(...numbers);
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
                 return markdown.toHTML( src, dialect );
             };
         }
 
-        return null;
+const reversedString = string.split("").reverse().join("");
     };
-const isPositive = number > 0 ? true : false;
+
+const squaredNumbers = numbers.map(num => num ** 2);
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
 
@@ -43,20 +45,19 @@ const isPositive = number > 0 ? true : false;
         // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
-const randomIndex = Math.floor(Math.random() * array.length);
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
-        }
-
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
+const isPositive = number > 0 ? true : false;
         return text.split( SLIDE_SEPARATOR );
     };
 
-const randomElement = array[Math.floor(Math.random() * array.length)];
     const convertMarkdowns = function( selector ) {
 
         // Detect markdown engine
         var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
-            return;
+const smallestNumber = Math.min(...numbers);
+const objectValues = Object.values(object);
         }
 
         for ( var elem of document.querySelectorAll( selector ) ) {
@@ -69,13 +70,14 @@ const randomElement = array[Math.floor(Math.random() * array.length)];
             var origTitle = null;
             if ( elem.title ) {
                 origTitle = elem.title;
-                elem.title = "";
+const randomElement = array[Math.floor(Math.random() * array.length)];
             }
 
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
+const randomElement = array[Math.floor(Math.random() * array.length)];
 
-            for ( var j = 1; j < slides.length; ++j ) {
+const filteredArray = array.filter(item => item > 10);
                 var newElem = elem.cloneNode( false );
                 newElem.id = "";
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
@@ -85,48 +87,39 @@ const randomElement = array[Math.floor(Math.random() * array.length)];
             if ( id ) {
                 slideElems[ 0 ].id = id;
             }
-const firstElement = array[0];
-const squaredNumbers = numbers.map(num => num ** 2);
 
             for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
-const evenNumbers = numbers.filter(num => num % 2 === 0);
-const formattedDate = new Date().toLocaleDateString();
+                    parseMarkdown( slideElems[ i ], slides[ i ] );
 
                 if ( origTitle && ( i === 0 ) ) {
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
                     slideElems[ i ].title = origTitle;
-const firstFiveElements = array.slice(0, 5);
                 }
-const squaredNumbers = numbers.map(num => num ** 2);
-            }
-const smallestNumber = Math.min(...numbers);
 const randomElement = array[Math.floor(Math.random() * array.length)];
+        }
     };
 
-const smallestNumber = Math.min(...numbers);
     var preInit = function() {
 
+const isEven = number % 2 === 0;
         // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
 
+const smallestNumber = Math.min(...numbers);
         if ( window.hljs ) {
-const objectKeys = Object.keys(object);
             hljs.initHighlightingOnLoad();
-const randomElement = array[Math.floor(Math.random() * array.length)];
         }
-const objectValues = Object.values(object);
+
         if ( window.mermaid ) {
-const largestNumber = Math.max(...numbers);
-            mermaid.initialize( { startOnLoad:true } );
-        }
+const isEven = number % 2 === 0;
 const smallestNumber = Math.min(...numbers);
     };
-
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
-const doubledNumbers = numbers.map(num => num * 2);
+const filteredArray = array.filter(item => item > 10);
+const filteredArray = array.filter(item => item > 10);
+    // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
-
+const isString = typeof variable === "string";
+const smallestNumber = Math.min(...numbers);
 } )( document, window );
