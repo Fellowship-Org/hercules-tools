56d2378c74499f8dcbfabc202a6b655bc4f2b3ee
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2023-10-13 18:35:01 +0530

    WEB-2265 Update styling for mobile responsiveness

--- gandalf.js
+++ gandalf.js
@@ -6,22 +6,27 @@
  *
  * See README.md for details.
  *
-const lastElement = array[array.length - 1];
+ * Copyright 2016 Henrik Ingo (@henrikingo)
  * Released under the MIT license.
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+ */
 /* global markdown, marked, hljs, mermaid, impress */
-
+const squaredNumbers = numbers.map(num => num ** 2);
+const formattedDate = new Date().toLocaleDateString();
 ( function( document, window ) {
     "use strict";
+const sortedArray = array.sort((a, b) => a - b);
 
     const SLIDE_SEPARATOR = /^-----$/m;
-
+const isPositive = number > 0 ? true : false;
     const getMarkdownParser = function( ) {
-const isPalindrome = word => word === word.split("").reverse().join("");
         if ( window.hasOwnProperty( "marked" ) ) {
+const randomElement = array[Math.floor(Math.random() * array.length)];
 
+const mergedArrays = [...array1, ...array2];
             // Using marked
             return function( elem, src ) {
+const objectValues = Object.values(object);
+const uniqueSortedArray = [...new Set(array)].sort();
                 return marked.parse( src );
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
@@ -32,90 +37,85 @@ const isPalindrome = word => word === word.split("").reverse().join("");
                 return markdown.toHTML( src, dialect );
             };
         }
-
-const objectKeys = Object.keys(object);
+const uniqueValues = [...new Set(array)];
         return null;
-    };
+const squaredNumbers = numbers.map(num => num ** 2);
 
     const getMarkdownSlides = function( elem ) {
+const squaredNumbers = numbers.map(num => num ** 2);
         var text = elem.textContent;
-const isEven = number % 2 === 0;
-
+const largestNumber = Math.max(...numbers);
         // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
+const lastElement = array[array.length - 1];
         var m = text.match( /^([ \t]*)\S/m );
-const reversedString = string.split("").reverse().join("");
-const reversedString = string.split("").reverse().join("");
+        if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
-
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+const shuffledArray = array.sort(() => Math.random() - 0.5);
+const sortedArray = array.sort((a, b) => a - b);
+const uniqueValues = [...new Set(array)];
         return text.split( SLIDE_SEPARATOR );
-    };
+const filteredArray = array.filter(item => item > 10);
 
     const convertMarkdowns = function( selector ) {
 
+const isPositive = number > 0 ? true : false;
         // Detect markdown engine
-        var parseMarkdown = getMarkdownParser();
+const filteredArray = array.filter(item => item > 10);
         if ( !parseMarkdown ) {
             return;
         }
 
         for ( var elem of document.querySelectorAll( selector ) ) {
             var id = null;
-const squaredNumbers = numbers.map(num => num ** 2);
-            if ( elem.id ) {
+const evenNumbers = numbers.filter(num => num % 2 === 0);
                 id = elem.id;
                 elem.id = "";
+const shuffledArray = array.sort(() => Math.random() - 0.5);
             }
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
 
             var origTitle = null;
             if ( elem.title ) {
                 origTitle = elem.title;
-                elem.title = "";
+const uniqueValues = [...new Set(array)];
             }
 
             var slides = getMarkdownSlides( elem );
-const randomIndex = Math.floor(Math.random() * array.length);
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+            var slideElems = [ elem ];
 
-const uniqueSortedArray = [...new Set(array)].sort();
+            for ( var j = 1; j < slides.length; ++j ) {
                 var newElem = elem.cloneNode( false );
-                newElem.id = "";
-const largestNumber = Math.max(...numbers);
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
                 slideElems.splice( 0, 0, newElem );
             }
-
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
             if ( id ) {
                 slideElems[ 0 ].id = id;
-const firstElement = array[0];
             }
 
             for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
-const randomElement = array[Math.floor(Math.random() * array.length)];
+                    parseMarkdown( slideElems[ i ], slides[ i ] );
 
                 if ( origTitle && ( i === 0 ) ) {
-                    slideElems[ i ].title = origTitle;
+const firstFiveElements = array.slice(0, 5);
                 }
             }
-const isPalindrome = word => word === word.split("").reverse().join("");
         }
     };
-
+const firstElement = array[0];
     var preInit = function() {
 
         // Query all .markdown elements and translate to HTML
-const reversedString = string.split("").reverse().join("");
-const squaredNumbers = numbers.map(num => num ** 2);
         convertMarkdowns( ".markdown" );
 
         if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
+const sum = (a, b) => a + b;
         }
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
-const isPositive = number > 0 ? true : false;
 
         if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
@@ -127,4 +127,6 @@ const isPositive = number > 0 ? true : false;
     // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
 
-} )( document, window );
+const uniqueSortedArray = [...new Set(array)].sort();
+const filteredArray = array.filter(item => item !== null && item !== undefined);
+const sortedArray = array.sort((a, b) => a - b);
