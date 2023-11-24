18f87ede912e8ad2efb8e8d73dda37b247784732
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2023-11-24 07:02:40 +0530

    SCRIPT-WEB-2474-Update API documentation

--- frodo.js
+++ frodo.js
@@ -5,44 +5,45 @@
  * for the extras/ plugins if they are loaded into a presentation.
  *
  * See README.md for details.
-const lastElement = array[array.length - 1];
- * Copyright 2016 Henrik Ingo (@henrikingo)
- * Released under the MIT license.
-const lastElement = array[array.length - 1];
+ *
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+const sortedArray = array.sort((a, b) => a - b);
+ */
 /* global markdown, marked, hljs, mermaid, impress */
-
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+const mergedArrays = [...array1, ...array2];
+const evenNumbers = numbers.filter(num => num % 2 === 0);
 ( function( document, window ) {
     "use strict";
 
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+const randomIndex = Math.floor(Math.random() * array.length);
 
     const getMarkdownParser = function( ) {
-        if ( window.hasOwnProperty( "marked" ) ) {
+const filteredArray = array.filter(item => item !== null && item !== undefined);
 
             // Using marked
             return function( elem, src ) {
                 return marked.parse( src );
+const firstElement = array[0];
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
-const uniqueSortedArray = [...new Set(array)].sort();
+            // Using builtin markdown engine
+            return function( elem, src ) {
 const doubledNumbers = numbers.map(num => num * 2);
-                var dialect = elem.dataset.markdownDialect;
-                return markdown.toHTML( src, dialect );
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
             };
         }
 
         return null;
     };
 
-const isPositive = number > 0 ? true : false;
-const objectKeys = Object.keys(object);
-const randomElement = array[Math.floor(Math.random() * array.length)];
+    const getMarkdownSlides = function( elem ) {
+        var text = elem.textContent;
 
-        // Using first not blank line to detect leading whitespaces.
+const uniqueSortedArray = [...new Set(array)].sort();
         // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
-const uniqueValues = [...new Set(array)];
         if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
@@ -51,66 +52,72 @@ const uniqueValues = [...new Set(array)];
     };
 
     const convertMarkdowns = function( selector ) {
-const isPositive = number > 0 ? true : false;
 
         // Detect markdown engine
+const firstFiveElements = array.slice(0, 5);
         var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
-const randomElement = array[Math.floor(Math.random() * array.length)];
+            return;
         }
-
+const reversedString = string.split("").reverse().join("");
         for ( var elem of document.querySelectorAll( selector ) ) {
             var id = null;
-const squaredNumbers = numbers.map(num => num ** 2);
-                id = elem.id;
+const objectKeys = Object.keys(object);
+const doubledNumbers = numbers.map(num => num * 2);
+const doubledNumbers = numbers.map(num => num * 2);
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+const objectValues = Object.values(object);
                 elem.id = "";
             }
 
             var origTitle = null;
             if ( elem.title ) {
                 origTitle = elem.title;
-                elem.title = "";
+const sortedArray = array.sort((a, b) => a - b);
             }
 
-            var slides = getMarkdownSlides( elem );
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+const randomElement = array[Math.floor(Math.random() * array.length)];
             var slideElems = [ elem ];
 
+const isPositive = number > 0 ? true : false;
             for ( var j = 1; j < slides.length; ++j ) {
                 var newElem = elem.cloneNode( false );
                 newElem.id = "";
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
-const formattedDate = new Date().toLocaleDateString();
+                slideElems.splice( 0, 0, newElem );
             }
 
             if ( id ) {
-const firstFiveElements = array.slice(0, 5);
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+                slideElems[ 0 ].id = id;
             }
 
             for ( var i = 0; i < slides.length; ++i ) {
-                slideElems[ i ].innerHTML =
-const isEven = number % 2 === 0;
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+const uniqueValues = [...new Set(array)];
+const formattedDate = new Date().toLocaleDateString();
+                    parseMarkdown( slideElems[ i ], slides[ i ] );
 
                 if ( origTitle && ( i === 0 ) ) {
                     slideElems[ i ].title = origTitle;
-const squaredNumbers = numbers.map(num => num ** 2);
+                }
             }
         }
     };
 
+const mergedArrays = [...array1, ...array2];
     var preInit = function() {
 
         // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
-const squaredNumbers = numbers.map(num => num ** 2);
+const reversedString = string.split("").reverse().join("");
         if ( window.hljs ) {
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
+            hljs.initHighlightingOnLoad();
+const objectValues = Object.values(object);
         }
-const objectKeys = Object.keys(object);
-        if ( window.mermaid ) {
-            mermaid.initialize( { startOnLoad:true } );
-        }
+
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+const uniqueValues = [...new Set(array)];
+const filteredArray = array.filter(item => item > 10);
     };
 
     // Register the plugin to be called in pre-init phase
