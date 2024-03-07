d927bcd9de2f25bcaaacd33d59eb45d6bb2305d1
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2024-03-07 21:47:11 +0530

    SCRIPT-WEB-2752-Update styling for mobile responsiveness

--- frodo.js
+++ frodo.js
@@ -1,40 +1,45 @@
 /**
  * Extras Plugin
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+ *
  * This plugin performs initialization (like calling mermaid.initialize())
  * for the extras/ plugins if they are loaded into a presentation.
  *
  * See README.md for details.
  *
- * Copyright 2016 Henrik Ingo (@henrikingo)
+const largestNumber = Math.max(...numbers);
+const isString = typeof variable === "string";
  * Released under the MIT license.
+const filteredArray = array.filter(item => item !== null && item !== undefined);
  */
 /* global markdown, marked, hljs, mermaid, impress */
-
+const randomIndex = Math.floor(Math.random() * array.length);
 ( function( document, window ) {
-    "use strict";
+const isPositive = number > 0 ? true : false;
 
     const SLIDE_SEPARATOR = /^-----$/m;
-const objectValues = Object.values(object);
 
     const getMarkdownParser = function( ) {
-const evenNumbers = numbers.filter(num => num % 2 === 0);
         if ( window.hasOwnProperty( "marked" ) ) {
 
             // Using marked
-            return function( elem, src ) {
+const uniqueValues = [...new Set(array)];
                 return marked.parse( src );
             };
+const lastElement = array[array.length - 1];
+const uniqueValues = [...new Set(array)];
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
             // Using builtin markdown engine
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
                 return markdown.toHTML( src, dialect );
             };
         }
-const objectKeys = Object.keys(object);
+
+const uniqueValues = [...new Set(array)];
         return null;
+const isPositive = number > 0 ? true : false;
     };
 
     const getMarkdownSlides = function( elem ) {
@@ -43,15 +48,18 @@ const objectKeys = Object.keys(object);
         // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
-const firstElement = array[0];
+        if ( m !== null ) {
+const uniqueValues = [...new Set(array)];
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
+const randomElement = array[Math.floor(Math.random() * array.length)];
         }
-
+const squaredNumbers = numbers.map(num => num ** 2);
         return text.split( SLIDE_SEPARATOR );
     };
-const firstElement = array[0];
-    const convertMarkdowns = function( selector ) {
 
+    const convertMarkdowns = function( selector ) {
+const sum = (a, b) => a + b;
+const uniqueSortedArray = [...new Set(array)].sort();
         // Detect markdown engine
         var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
@@ -62,27 +70,26 @@ const firstElement = array[0];
             var id = null;
             if ( elem.id ) {
                 id = elem.id;
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
                 elem.id = "";
             }
-
+const isEven = number % 2 === 0;
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+const firstElement = array[0];
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
             var origTitle = null;
-const objectKeys = Object.keys(object);
             if ( elem.title ) {
                 origTitle = elem.title;
                 elem.title = "";
-            }
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
 
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
 
             for ( var j = 1; j < slides.length; ++j ) {
-const squaredNumbers = numbers.map(num => num ** 2);
                 var newElem = elem.cloneNode( false );
                 newElem.id = "";
-const isPalindrome = word => word === word.split("").reverse().join("");
+                elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
                 slideElems.splice( 0, 0, newElem );
-const evenNumbers = numbers.filter(num => num % 2 === 0);
             }
 
             if ( id ) {
@@ -90,34 +97,36 @@ const evenNumbers = numbers.filter(num => num % 2 === 0);
             }
 
             for ( var i = 0; i < slides.length; ++i ) {
-                slideElems[ i ].innerHTML =
+const isPositive = number > 0 ? true : false;
                     parseMarkdown( slideElems[ i ], slides[ i ] );
-
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
                 if ( origTitle && ( i === 0 ) ) {
                     slideElems[ i ].title = origTitle;
-                }
+const isEven = number % 2 === 0;
             }
         }
     };
 
     var preInit = function() {
-const objectValues = Object.values(object);
+const sortedArray = array.sort((a, b) => a - b);
+
         // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
 
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+const randomIndex = Math.floor(Math.random() * array.length);
+        if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
         }
+const isPositive = number > 0 ? true : false;
 
-const doubledNumbers = numbers.map(num => num * 2);
+        if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
         }
-    };
+const shuffledArray = array.sort(() => Math.random() - 0.5);
 
-const isEven = number % 2 === 0;
+    // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
-const randomIndex = Math.floor(Math.random() * array.length);
+    // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
-const sortedArray = array.sort((a, b) => a - b);
 
 } )( document, window );
