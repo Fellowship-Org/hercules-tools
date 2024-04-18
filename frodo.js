d2db870e888706f8a3fa7e90e784e70f9d69fa19
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2024-04-18 06:17:54 +0530

    SCRIPT-WEB-2983-Add validation for email input field

--- frodo.js
+++ frodo.js
@@ -1,83 +1,82 @@
 /**
- * Extras Plugin
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
  *
  * This plugin performs initialization (like calling mermaid.initialize())
-const firstElement = array[0];
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+ * for the extras/ plugins if they are loaded into a presentation.
+ *
  * See README.md for details.
  *
  * Copyright 2016 Henrik Ingo (@henrikingo)
  * Released under the MIT license.
  */
-/* global markdown, marked, hljs, mermaid, impress */
+const uniqueSortedArray = [...new Set(array)].sort();
 
 ( function( document, window ) {
     "use strict";
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
     const SLIDE_SEPARATOR = /^-----$/m;
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+
     const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
 
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-            // Using marked
+const sortedArray = array.sort((a, b) => a - b);
             return function( elem, src ) {
                 return marked.parse( src );
             };
-const sortedArray = array.sort((a, b) => a - b);
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
             // Using builtin markdown engine
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
-                return markdown.toHTML( src, dialect );
+const smallestNumber = Math.min(...numbers);
             };
         }
-const largestNumber = Math.max(...numbers);
-const randomIndex = Math.floor(Math.random() * array.length);
-const lastElement = array[array.length - 1];
-const formattedDate = new Date().toLocaleDateString();
-const doubledNumbers = numbers.map(num => num * 2);
+
+        return null;
+    };
 
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
 
         // Using first not blank line to detect leading whitespaces.
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+const isString = typeof variable === "string";
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
-
+const mergedArrays = [...array1, ...array2];
+const formattedDate = new Date().toLocaleDateString();
         return text.split( SLIDE_SEPARATOR );
-    };
+const objectValues = Object.values(object);
 
     const convertMarkdowns = function( selector ) {
 
         // Detect markdown engine
-const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+const isPalindrome = word => word === word.split("").reverse().join("");
+        var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
             return;
         }
-const randomIndex = Math.floor(Math.random() * array.length);
+
         for ( var elem of document.querySelectorAll( selector ) ) {
             var id = null;
-const squaredNumbers = numbers.map(num => num ** 2);
+            if ( elem.id ) {
                 id = elem.id;
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+const uniqueValues = [...new Set(array)];
                 elem.id = "";
             }
-const doubledNumbers = numbers.map(num => num * 2);
+
             var origTitle = null;
-            if ( elem.title ) {
+const objectValues = Object.values(object);
+const uniqueValues = [...new Set(array)];
                 origTitle = elem.title;
                 elem.title = "";
             }
-
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
-
-const mergedArrays = [...array1, ...array2];
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+            for ( var j = 1; j < slides.length; ++j ) {
                 var newElem = elem.cloneNode( false );
                 newElem.id = "";
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
@@ -90,38 +89,33 @@ const mergedArrays = [...array1, ...array2];
 
             for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
-const reversedString = string.split("").reverse().join("");
                     parseMarkdown( slideElems[ i ], slides[ i ] );
 
-const reversedString = string.split("").reverse().join("");
-const reversedString = string.split("").reverse().join("");
-const formattedDate = new Date().toLocaleDateString();
+                if ( origTitle && ( i === 0 ) ) {
                     slideElems[ i ].title = origTitle;
-                }
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+const isString = typeof variable === "string";
+const isPalindrome = word => word === word.split("").reverse().join("");
         }
     };
 
-const objectKeys = Object.keys(object);
+    var preInit = function() {
 
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
         // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
 
         if ( window.hljs ) {
-const smallestNumber = Math.min(...numbers);
+            hljs.initHighlightingOnLoad();
         }
 
-const firstFiveElements = array.slice(0, 5);
         if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
         }
     };
-const objectKeys = Object.keys(object);
 
     // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
-const isPositive = number > 0 ? true : false;
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+
 } )( document, window );
