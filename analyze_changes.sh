e800a4de69ac1768467292fbb8ffd7acd736c9c2
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2024-04-26 16:18:01 +0530

    test Update analyze_changes.sh

--- analyze_changes.sh
+++ analyze_changes.sh
@@ -4,29 +4,23 @@ analyse_pr_changes() {
     local base_ref="$1"
     local head_ref="$2"
     
-    if [ -d ".git" ]; then
-        echo "Git repository found."
-    else
-        echo "Not a Git repository. Exiting."
-        exit 1
-    fi
-    git log
-
-    ref_exists() {
-        git rev-parse --quiet --verify "$1" >/dev/null
-    }
-
-    # Check if the base reference exists
-    if ! ref_exists "$base_ref"; then
-        echo "Base reference '$base_ref' does not exist. Exiting."
-        exit 1
+    # Get the commit range
+    commit_range="${base_ref}..${head_ref}"
+    
+    # Check if base_ref points to the initial commit
+    initial_commit=$(git rev-list --max-parents=0 HEAD)
+    if [ "${base_ref}" = "${initial_commit}" ]; then
+        commit_range="${head_ref}"
     fi
     
-    # Check if the head reference exists
-    if ! ref_exists "$head_ref"; then
-        echo "Head reference '$head_ref' does not exist. Exiting."
-        exit 1
-    fi
+    # Get the list of changed files
+    changed_files_with_commits=$(git log --pretty=format:"%h" --name-only "${commit_range}")
+    
+    # Output the list of changed files with commits
+    echo "changed_files_with_commits:"
+    echo "$changed_files_with_commits"
+
+    
 }
 main() {
     if [ $# -ne 2 ]; then
