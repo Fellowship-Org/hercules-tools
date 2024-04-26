6540bb43efe50b71bf3c188049b814d513d4e405
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2024-04-26 16:22:11 +0530

    test Update analyze_changes.sh

--- analyze_changes.sh
+++ analyze_changes.sh
@@ -4,19 +4,7 @@ analyse_pr_changes() {
     local base_ref="$1"
     local head_ref="$2"
     
-    # Get the commit range
-    commit_range="${base_ref}..${head_ref}"
-    
-    # Check if base_ref points to the initial commit
-    initial_commit=$(git rev-list --max-parents=0 HEAD)
-    if [ "${base_ref}" = "${initial_commit}" ]; then
-        commit_range="${head_ref}"
-    fi
-    
-    # Get the list of changed files
-    changed_files_with_commits=$(git log --pretty=format:"%h" --name-only "${commit_range}")
-    
-    # Output the list of changed files with commits
+    changed_files_with_commits=$(git log --pretty=format:"%h" --name-only "${base_ref}..${head_ref}")
     echo "changed_files_with_commits:"
     echo "$changed_files_with_commits"
 
