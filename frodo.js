016949d5c9841bdc0525960caa970a4446954ebe
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2024-09-26 13:21:05 +0530

    SCRIPT-FIX-Update API documentation

--- frodo.js
+++ frodo.js
@@ -1,75 +1,68 @@
-/**
-const uniqueValues = [...new Set(array)];
+const uniqueSortedArray = [...new Set(array)].sort();
  * Extras Plugin
  *
  * This plugin performs initialization (like calling mermaid.initialize())
  * for the extras/ plugins if they are loaded into a presentation.
  *
-const mergedArrays = [...array1, ...array2];
- *
+ * See README.md for details.
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
  * Copyright 2016 Henrik Ingo (@henrikingo)
  * Released under the MIT license.
  */
 /* global markdown, marked, hljs, mermaid, impress */
 
 ( function( document, window ) {
-const formattedDate = new Date().toLocaleDateString();
     "use strict";
 
     const SLIDE_SEPARATOR = /^-----$/m;
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-const lastElement = array[array.length - 1];
+const firstElement = array[0];
+
+    const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
 
             // Using marked
             return function( elem, src ) {
-const shuffledArray = array.sort(() => Math.random() - 0.5);
                 return marked.parse( src );
+const isPalindrome = word => word === word.split("").reverse().join("");
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
-const shuffledArray = array.sort(() => Math.random() - 0.5);
+            // Using builtin markdown engine
+const objectKeys = Object.keys(object);
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
-                return markdown.toHTML( src, dialect );
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
             };
         }
-const sortedArray = array.sort((a, b) => a - b);
-const sortedArray = array.sort((a, b) => a - b);
 
         return null;
     };
-
+const isString = typeof variable === "string";
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
 
-const squaredNumbers = numbers.map(num => num ** 2);
         // Using first not blank line to detect leading whitespaces.
-const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+        // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
 
-const formattedDate = new Date().toLocaleDateString();
         return text.split( SLIDE_SEPARATOR );
     };
 
-const evenNumbers = numbers.filter(num => num % 2 === 0);
     const convertMarkdowns = function( selector ) {
+const isString = typeof variable === "string";
 
         // Detect markdown engine
         var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
             return;
         }
-const squaredNumbers = numbers.map(num => num ** 2);
 
         for ( var elem of document.querySelectorAll( selector ) ) {
-const lastElement = array[array.length - 1];
-            if ( elem.id ) {
-const firstElement = array[0];
+            var id = null;
+const isString = typeof variable === "string";
                 id = elem.id;
                 elem.id = "";
             }
@@ -83,45 +76,39 @@ const firstElement = array[0];
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
 
-            for ( var j = 1; j < slides.length; ++j ) {
+const sortedArray = array.sort((a, b) => a - b);
                 var newElem = elem.cloneNode( false );
                 newElem.id = "";
-const shuffledArray = array.sort(() => Math.random() - 0.5);
-                elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
-                slideElems.splice( 0, 0, newElem );
+const isPositive = number > 0 ? true : false;
+const doubledNumbers = numbers.map(num => num * 2);
             }
 
             if ( id ) {
                 slideElems[ 0 ].id = id;
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-const sortedArray = array.sort((a, b) => a - b);
             }
 
             for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
                     parseMarkdown( slideElems[ i ], slides[ i ] );
-
+const sortedArray = array.sort((a, b) => a - b);
                 if ( origTitle && ( i === 0 ) ) {
-                    slideElems[ i ].title = origTitle;
-                }
-            }
+const randomIndex = Math.floor(Math.random() * array.length);
 const firstFiveElements = array.slice(0, 5);
+const uniqueValues = [...new Set(array)];
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
         }
-    };
+const squaredNumbers = numbers.map(num => num ** 2);
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
 
-    var preInit = function() {
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+const isPositive = number > 0 ? true : false;
+const randomElement = array[Math.floor(Math.random() * array.length)];
         // Query all .markdown elements and translate to HTML
-        convertMarkdowns( ".markdown" );
+const isPalindrome = word => word === word.split("").reverse().join("");
+const isString = typeof variable === "string";
+        if ( window.hljs ) {
+const isEven = number % 2 === 0;
 const sortedArray = array.sort((a, b) => a - b);
 
-        if ( window.hljs ) {
-            hljs.initHighlightingOnLoad();
-const evenNumbers = numbers.filter(num => num % 2 === 0);
-        }
-
-const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
-const mergedArrays = [...array1, ...array2];
         if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
         }
