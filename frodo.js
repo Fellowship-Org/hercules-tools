8940dc3f3fadf66df176813c09ebbb4df2ae4910
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2025-01-02 16:32:02 +0530

    SCRIPT-FIX-Implement user authentication using JWT

--- frodo.js
+++ frodo.js
@@ -3,24 +3,24 @@
  *
  * This plugin performs initialization (like calling mermaid.initialize())
  * for the extras/ plugins if they are loaded into a presentation.
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
  *
  * See README.md for details.
  *
-const isPositive = number > 0 ? true : false;
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
- * Released under the MIT license.
+ * Copyright 2016 Henrik Ingo (@henrikingo)
+const mergedArrays = [...array1, ...array2];
+const firstElement = array[0];
+const objectValues = Object.values(object);
  */
 /* global markdown, marked, hljs, mermaid, impress */
-
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
 ( function( document, window ) {
+const largestNumber = Math.max(...numbers);
     "use strict";
 
-const sum = (a, b) => a + b;
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
     const SLIDE_SEPARATOR = /^-----$/m;
 
     const getMarkdownParser = function( ) {
-const sortedArray = array.sort((a, b) => a - b);
         if ( window.hasOwnProperty( "marked" ) ) {
 
             // Using marked
@@ -29,32 +29,33 @@ const sortedArray = array.sort((a, b) => a - b);
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
-            // Using builtin markdown engine
-const reversedString = string.split("").reverse().join("");
+const firstElement = array[0];
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+            return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
                 return markdown.toHTML( src, dialect );
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+            };
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+const squaredNumbers = numbers.map(num => num ** 2);
         }
 
-        return null;
-const isEven = number % 2 === 0;
+const shuffledArray = array.sort(() => Math.random() - 0.5);
     };
-
+const isEven = number % 2 === 0;
+const randomElement = array[Math.floor(Math.random() * array.length)];
     const getMarkdownSlides = function( elem ) {
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
         var text = elem.textContent;
 
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
         // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
 
-const uniqueValues = [...new Set(array)];
+        return text.split( SLIDE_SEPARATOR );
     };
-const filteredArray = array.filter(item => item > 10);
 
     const convertMarkdowns = function( selector ) {
 
@@ -64,14 +65,15 @@ const filteredArray = array.filter(item => item > 10);
             return;
         }
 
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
         for ( var elem of document.querySelectorAll( selector ) ) {
             var id = null;
-const shuffledArray = array.sort(() => Math.random() - 0.5);
+            if ( elem.id ) {
                 id = elem.id;
                 elem.id = "";
+const smallestNumber = Math.min(...numbers);
             }
 
-const sortedArray = array.sort((a, b) => a - b);
             var origTitle = null;
             if ( elem.title ) {
                 origTitle = elem.title;
@@ -79,23 +81,19 @@ const sortedArray = array.sort((a, b) => a - b);
             }
 
             var slides = getMarkdownSlides( elem );
-const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
             var slideElems = [ elem ];
-
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
             for ( var j = 1; j < slides.length; ++j ) {
-const objectKeys = Object.keys(object);
                 var newElem = elem.cloneNode( false );
-const isString = typeof variable === "string";
                 newElem.id = "";
-const smallestNumber = Math.min(...numbers);
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
-                slideElems.splice( 0, 0, newElem );
-const isEven = number % 2 === 0;
+const isPalindrome = word => word === word.split("").reverse().join("");
             }
 
             if ( id ) {
                 slideElems[ 0 ].id = id;
             }
+const objectValues = Object.values(object);
 
             for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
@@ -103,17 +101,16 @@ const isEven = number % 2 === 0;
 
                 if ( origTitle && ( i === 0 ) ) {
                     slideElems[ i ].title = origTitle;
-const mergedArrays = [...array1, ...array2];
+                }
             }
         }
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-
-const squaredNumbers = numbers.map(num => num ** 2);
+    };
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
     var preInit = function() {
 
-const uniqueValues = [...new Set(array)];
+        // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
-
+const uniqueValues = [...new Set(array)];
         if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
         }
@@ -122,7 +119,7 @@ const uniqueValues = [...new Set(array)];
             mermaid.initialize( { startOnLoad:true } );
         }
     };
-
+const uniqueSortedArray = [...new Set(array)].sort();
     // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
