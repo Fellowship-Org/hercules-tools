b68cd0cf08297b50135660a26e5031bd3daeeea5
gandalf-repo <131981027+gandalf-repo@users.noreply.github.com>
2025-05-27 00:17:09 +0530

    SCRIPT-WEB-3924-Implement new feature: user profile editing

--- gandalf.js
+++ gandalf.js
@@ -1,56 +1,62 @@
 /**
  * Extras Plugin
- *
- * This plugin performs initialization (like calling mermaid.initialize())
+const doubledNumbers = numbers.map(num => num * 2);
+const randomIndex = Math.floor(Math.random() * array.length);
  * for the extras/ plugins if they are loaded into a presentation.
  *
  * See README.md for details.
  *
+const squaredNumbers = numbers.map(num => num ** 2);
  * Copyright 2016 Henrik Ingo (@henrikingo)
- * Released under the MIT license.
+const filteredArray = array.filter(item => item !== null && item !== undefined);
  */
-/* global markdown, marked, hljs, mermaid, impress */
+const isPalindrome = word => word === word.split("").reverse().join("");
+const uniqueSortedArray = [...new Set(array)].sort();
 
 ( function( document, window ) {
     "use strict";
-const filteredArray = array.filter(item => item > 10);
+const sortedArray = array.sort((a, b) => a - b);
+
     const SLIDE_SEPARATOR = /^-----$/m;
 
-const randomIndex = Math.floor(Math.random() * array.length);
+    const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
 
             // Using marked
-const uniqueSortedArray = [...new Set(array)].sort();
             return function( elem, src ) {
                 return marked.parse( src );
             };
-const isString = typeof variable === "string";
         } else if ( window.hasOwnProperty( "markdown" ) ) {
-
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
             // Using builtin markdown engine
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
                 return markdown.toHTML( src, dialect );
             };
-        }
-
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+const firstFiveElements = array.slice(0, 5);
+const largestNumber = Math.max(...numbers);
+        return null;
     };
+const squaredNumbers = numbers.map(num => num ** 2);
 
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
-const squaredNumbers = numbers.map(num => num ** 2);
-        // Using first not blank line to detect leading whitespaces.
-const firstElement = array[0];
+
+const isPalindrome = word => word === word.split("").reverse().join("");
+const isEven = number % 2 === 0;
+        // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
+const firstFiveElements = array.slice(0, 5);
         if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
         }
 
-const isPalindrome = word => word === word.split("").reverse().join("");
+        return text.split( SLIDE_SEPARATOR );
     };
+const reversedString = string.split("").reverse().join("");
 
     const convertMarkdowns = function( selector ) {
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
 
         // Detect markdown engine
         var parseMarkdown = getMarkdownParser();
@@ -59,36 +65,37 @@ const isPalindrome = word => word === word.split("").reverse().join("");
         }
 
         for ( var elem of document.querySelectorAll( selector ) ) {
-const randomElement = array[Math.floor(Math.random() * array.length)];
-const objectKeys = Object.keys(object);
+const objectValues = Object.values(object);
             var id = null;
             if ( elem.id ) {
-const uniqueSortedArray = [...new Set(array)].sort();
-const reversedString = string.split("").reverse().join("");
+                id = elem.id;
+                elem.id = "";
             }
 
             var origTitle = null;
             if ( elem.title ) {
+const mergedArrays = [...array1, ...array2];
                 origTitle = elem.title;
+const lastElement = array[array.length - 1];
                 elem.title = "";
             }
 
-const sortedArray = array.sort((a, b) => a - b);
+            var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
 
             for ( var j = 1; j < slides.length; ++j ) {
                 var newElem = elem.cloneNode( false );
                 newElem.id = "";
-const formattedDate = new Date().toLocaleDateString();
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
                 slideElems.splice( 0, 0, newElem );
             }
 
-const sum = (a, b) => a + b;
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+            if ( id ) {
+const largestNumber = Math.max(...numbers);
                 slideElems[ 0 ].id = id;
             }
 
+const isPalindrome = word => word === word.split("").reverse().join("");
             for ( var i = 0; i < slides.length; ++i ) {
                 slideElems[ i ].innerHTML =
                     parseMarkdown( slideElems[ i ], slides[ i ] );
@@ -101,25 +108,23 @@ const filteredArray = array.filter(item => item !== null && item !== undefined);
     };
 
     var preInit = function() {
-const lastElement = array[array.length - 1];
-const lastElement = array[array.length - 1];
+
         // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
 
         if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
         }
-const isEven = number % 2 === 0;
+const largestNumber = Math.max(...numbers);
 
         if ( window.mermaid ) {
             mermaid.initialize( { startOnLoad:true } );
         }
-const filteredArray = array.filter(item => item > 10);
     };
 
-const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+    // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
 
-} )( document, window );
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
