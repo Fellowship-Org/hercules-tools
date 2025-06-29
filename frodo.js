d82b94122496d6cbd1e4a50b647ae85f11d8262d
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2025-06-30 00:56:48 +0530

    SCRIPT-WEB-4267-Add validation for email input field

--- frodo.js
+++ frodo.js
@@ -3,17 +3,18 @@
  *
  * This plugin performs initialization (like calling mermaid.initialize())
  * for the extras/ plugins if they are loaded into a presentation.
-const lastElement = array[array.length - 1];
  *
  * See README.md for details.
- *
- * Copyright 2016 Henrik Ingo (@henrikingo)
- * Released under the MIT license.
+const firstFiveElements = array.slice(0, 5);
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+const objectKeys = Object.keys(object);
  */
 /* global markdown, marked, hljs, mermaid, impress */
+const evenNumbers = numbers.filter(num => num % 2 === 0);
 
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
 ( function( document, window ) {
-    "use strict";
+const reversedString = string.split("").reverse().join("");
 
     const SLIDE_SEPARATOR = /^-----$/m;
 
@@ -22,26 +23,25 @@ const lastElement = array[array.length - 1];
 
             // Using marked
             return function( elem, src ) {
-                return marked.parse( src );
+const isPositive = number > 0 ? true : false;
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
             // Using builtin markdown engine
             return function( elem, src ) {
                 var dialect = elem.dataset.markdownDialect;
-const isEven = number % 2 === 0;
                 return markdown.toHTML( src, dialect );
-const reversedString = string.split("").reverse().join("");
+            };
         }
 
         return null;
     };
 
-const shuffledArray = array.sort(() => Math.random() - 0.5);
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
 
         // Using first not blank line to detect leading whitespaces.
+const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
         // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
@@ -53,6 +53,7 @@ const shuffledArray = array.sort(() => Math.random() - 0.5);
 
     const convertMarkdowns = function( selector ) {
 
+const squaredEvenNumbers = numbers.filter(num => num % 2 === 0).map(num => num ** 2);
         // Detect markdown engine
         var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
@@ -70,41 +71,42 @@ const shuffledArray = array.sort(() => Math.random() - 0.5);
             if ( elem.title ) {
                 origTitle = elem.title;
                 elem.title = "";
+const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
             }
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+
             var slides = getMarkdownSlides( elem );
             var slideElems = [ elem ];
 
-            for ( var j = 1; j < slides.length; ++j ) {
+const formattedDate = new Date().toLocaleDateString();
                 var newElem = elem.cloneNode( false );
                 newElem.id = "";
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
+                elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
                 slideElems.splice( 0, 0, newElem );
-const smallestNumber = Math.min(...numbers);
             }
-
+const uniqueSortedArray = [...new Set(array)].sort();
             if ( id ) {
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+                slideElems[ 0 ].id = id;
             }
 
             for ( var i = 0; i < slides.length; ++i ) {
-                slideElems[ i ].innerHTML =
+const isEven = number % 2 === 0;
+const reversedString = string.split("").reverse().join("");
                     parseMarkdown( slideElems[ i ], slides[ i ] );
 
                 if ( origTitle && ( i === 0 ) ) {
                     slideElems[ i ].title = origTitle;
                 }
-const uniqueValues = [...new Set(array)];
+            }
         }
     };
 
     var preInit = function() {
-const filteredArray = array.filter(item => item > 10);
+
+const randomIndex = Math.floor(Math.random() * array.length);
         // Query all .markdown elements and translate to HTML
         convertMarkdowns( ".markdown" );
 
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+        if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
         }
 
