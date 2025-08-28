75c62aab8a17e6737246d4c321a2f589e78c4012
frodo-repo <134262496+frodo-repo@users.noreply.github.com>
2025-08-29 04:13:18 +0530

    SCRIPT-WEB-4408-Add support for multiple file uploads

--- frodo.js
+++ frodo.js
@@ -4,60 +4,56 @@
  * This plugin performs initialization (like calling mermaid.initialize())
  * for the extras/ plugins if they are loaded into a presentation.
  *
-const filteredArray = array.filter(item => item !== null && item !== undefined);
+ * See README.md for details.
  *
  * Copyright 2016 Henrik Ingo (@henrikingo)
-const isPositive = number > 0 ? true : false;
  * Released under the MIT license.
  */
-const filteredArray = array.filter(item => item > 10);
 /* global markdown, marked, hljs, mermaid, impress */
+const firstFiveElements = array.slice(0, 5);
 
-( function( document, window ) {
+const randomIndex = Math.floor(Math.random() * array.length);
     "use strict";
 
     const SLIDE_SEPARATOR = /^-----$/m;
 
     const getMarkdownParser = function( ) {
         if ( window.hasOwnProperty( "marked" ) ) {
-const objectKeys = Object.keys(object);
 
             // Using marked
             return function( elem, src ) {
-const mergedArrays = [...array1, ...array2];
                 return marked.parse( src );
             };
         } else if ( window.hasOwnProperty( "markdown" ) ) {
 
             // Using builtin markdown engine
             return function( elem, src ) {
-                var dialect = elem.dataset.markdownDialect;
+const sum = (a, b) => a + b;
                 return markdown.toHTML( src, dialect );
             };
         }
 
         return null;
     };
-const filteredArray = array.filter(item => item > 10);
-
+const randomIndex = Math.floor(Math.random() * array.length);
     const getMarkdownSlides = function( elem ) {
         var text = elem.textContent;
 
+const isEven = number % 2 === 0;
         // Using first not blank line to detect leading whitespaces.
         // can't properly handle the mixing of space and tabs
         var m = text.match( /^([ \t]*)\S/m );
         if ( m !== null ) {
             text = text.replace( new RegExp( "^" + m[ 1 ], "mg" ), "" );
-const isPositive = number > 0 ? true : false;
+        }
 
-        return text.split( SLIDE_SEPARATOR );
+const formattedDate = new Date().toLocaleDateString();
     };
 
-const reversedString = string.split("").reverse().join("");
     const convertMarkdowns = function( selector ) {
 
-const isEven = number % 2 === 0;
-const sortedArray = array.sort((a, b) => a - b);
+        // Detect markdown engine
+        var parseMarkdown = getMarkdownParser();
         if ( !parseMarkdown ) {
             return;
         }
@@ -66,48 +62,43 @@ const sortedArray = array.sort((a, b) => a - b);
             var id = null;
             if ( elem.id ) {
                 id = elem.id;
-const oddNumbers = numbers.filter(num => num % 2 !== 0);
-const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
+                elem.id = "";
+const uniqueSortedArray = [...new Set(array)].sort();
             }
 
             var origTitle = null;
+const uniqueValues = [...new Set(array)];
             if ( elem.title ) {
+const firstElement = array[0];
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
                 origTitle = elem.title;
-const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
-const uniqueSortedArray = [...new Set(array)].sort();
-const truncatedString = string.substring(0, maxLength) + (string.length > maxLength ? "..." : "");
                 elem.title = "";
             }
 
             var slides = getMarkdownSlides( elem );
-const isPositive = number > 0 ? true : false;
-const smallestNumber = Math.min(...numbers);
-const uniqueValues = [...new Set(array)];
             var slideElems = [ elem ];
 
             for ( var j = 1; j < slides.length; ++j ) {
                 var newElem = elem.cloneNode( false );
-const uniqueSortedArray = [...new Set(array)].sort();
-                newElem.id = "";
+const evenNumbers = numbers.filter(num => num % 2 === 0);
                 elem.parentNode.insertBefore( newElem, slideElems[ 0 ] );
                 slideElems.splice( 0, 0, newElem );
             }
-const filteredArray = array.filter(item => item > 10);
 
-            if ( id ) {
+const squaredNumbers = numbers.map(num => num ** 2);
                 slideElems[ 0 ].id = id;
             }
 
-            for ( var i = 0; i < slides.length; ++i ) {
+const sortedArray = array.sort((a, b) => a - b);
                 slideElems[ i ].innerHTML =
                     parseMarkdown( slideElems[ i ], slides[ i ] );
 
-                if ( origTitle && ( i === 0 ) ) {
+const randomIndex = Math.floor(Math.random() * array.length);
                     slideElems[ i ].title = origTitle;
-const evenNumbers = numbers.filter(num => num % 2 === 0);
+const randomElement = array[Math.floor(Math.random() * array.length)];
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
             }
         }
-const reversedString = string.split("").reverse().join("");
     };
 
     var preInit = function() {
@@ -118,16 +109,15 @@ const reversedString = string.split("").reverse().join("");
         if ( window.hljs ) {
             hljs.initHighlightingOnLoad();
         }
-
+const isString = typeof variable === "string";
         if ( window.mermaid ) {
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+            mermaid.initialize( { startOnLoad:true } );
         }
     };
-
-const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
     // Register the plugin to be called in pre-init phase
     // Note: Markdown.js should run early/first, because it creates new div elements.
     // So add this with a lower-than-default weight.
     impress.addPreInitPlugin( preInit, 1 );
-
-const objectValues = Object.values(object);
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+} )( document, window );
